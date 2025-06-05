314c9bf92b3a901377e04824f6e08f331f72d677
MarcusO17 <marcusonggeye@gmail.com>
2025-06-05 14:02:40 +0800

    Refactor: Created reusable BottomSheetButton widget
    This commit introduces a new `BottomSheetButton` widget to reduce code duplication in `BottomScannedFileSheet`.
    
    The `BottomScannedFileSheet` now utilizes `BottomSheetButton` for its "Take Photo" and "Choose From Gallery" options, simplifying its structure and improving maintainability.

--- lib/widgets/bottom_scanned_file_sheet.dart
+++ lib/widgets/bottom_scanned_file_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bottom_sheet_button.dart';
 
 class BottomScannedFileSheet extends StatelessWidget {
   const BottomScannedFileSheet({super.key});
@@ -8,64 +9,40 @@ class BottomScannedFileSheet extends StatelessWidget {
     return SizedBox(
       height: 200,
       width: double.infinity,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 40),
-            Padding(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 30),
+          Padding(
               padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: (){
+              child: BottomSheetButton(
+                text: "Take Photo",
+                icon: Icons.camera_enhance,
+                onPressed: () {
                   Navigator.pushNamed(context, '/camera');
                 },
-                child: Container(
-                  width: double.infinity,
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 45),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                         child: Icon(Icons.camera)),
-                      ),
-                      Align(
-                        alignment:Alignment.center,
-                          child: const Text('Take Photo'))
-                    ],
-                  ),
-                ),
-              ),
+              )),
+          Divider(
+            color: Colors.grey[600], // Color of the line
+            thickness: 1, // Thickness of the line
+            height:
+                20, // Total height of the Divider widget (includes space above/below)
+            indent: 30, // Space to the left of the line
+            endIndent: 30, // Space to the right of the line
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top:14,left: 8),
+            child: BottomSheetButton(
+              text: "Choose From Gallery",
+              icon: Icons.photo_album,
+              onPressed: () {
+                print('Choosed!');
+              },
             ),
-            Divider(
-              color: Colors.grey[600], // Color of the line
-              thickness: 1,         // Thickness of the line
-              height: 20,           // Total height of the Divider widget (includes space above/below)
-              indent: 30,           // Space to the left of the line
-              endIndent: 30,        // Space to the right of the line
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child:Container(
-                width: double.infinity,
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 45),
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Icon(Icons.photo_album)),
-                    ),
-                    Align(
-                        alignment:Alignment.center,
-                        child: const Text('Choose From Gallery'))
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
