90ea685ac1cfe6584822a03dec1e21b43fda87db
MarcusO17 <marcusonggeye@gmail.com>
2025-06-15 15:52:05 +0800

    Add cleartext traffic permission, update ApiService for receipt upload, and modify camera page to use ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'auth_service.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ApiService {
   final apiUrl = '10.0.2.2:8000';
@@ -22,6 +23,27 @@ class ApiService {
       return response.body;
     } else {
       throw Exception('Failed to load data');
+    }
+  }
+
+  Future<void> uploadReceipt(String? idToken, String imagePath) async {
+    if (idToken == null || idToken.isEmpty) {
+      throw Exception('ID token is required to add a receipt.');
+    }
+
+    // Send id_token as a query parameter
+    var url = Uri.http(apiUrl, '/upload-reciept-image/');
+    var request = http.MultipartRequest('POST', url);
+
+    var multipartFile = await http.MultipartFile.fromPath('file', imagePath);
+    request.files.add(multipartFile);
+
+    try {
+      http.StreamedResponse response = await request.send();
+      String responseBody = await response.stream.bytesToString();
+      print(responseBody);
+    } catch (e) {
+      throw Exception('Error sending request to add receipt: $e');
     }
   }
 }
